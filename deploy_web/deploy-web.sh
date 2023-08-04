4b08803b1307057bc716f554567e355abfb7faf1
izaias-junior <izaias.junior@hp.com>
2023-08-04 00:46:58 -0300

    add build for deploy

--- deploy_web/deploy-web.sh
+++ deploy_web/deploy-web.sh
@@ -1,1 +1,8 @@
-cp -r build/web ../tock-web-app/
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "script is located in " $SCRIPT_DIR
+echo "copping files"
+
+cp -rf $SCRIPT_DIR/../build/web $SCRIPT_DIR/../tock-web-app/
+gitcommand= git add . && git commit -m "add build for deploy"
