826fbfdd25376da9cd88f8928f8574c2fd368f7d
izaias-junior <izaias.junior@hp.com>
2023-08-04 00:51:40 -0300

    adjustment in sh to deploy

--- deploy_web/deploy-web.sh
+++ deploy_web/deploy-web.sh
@@ -6,6 +6,9 @@ echo "script is located in " $SCRIPT_DIR
 echo "copping files ..."
 cp -rf $SCRIPT_DIR/../build/web $SCRIPT_DIR/../tock-web-app/
 
-echo "commiting and push ..."
+
+
+echo "commiting tock-web-app and push ..."
+cd  $SCRIPT_DIR/../tock-web-app/
 gitcommand= git add . && git commit -m "automatic commit to deploy build" && git push 
 
