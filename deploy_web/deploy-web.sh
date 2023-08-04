ccd78f1b01d92c0e37ec8f371163db39fd536428
izaias-junior <izaias.junior@hp.com>
2023-08-04 00:48:56 -0300

    automatic commit to deploy build

--- deploy_web/deploy-web.sh
+++ deploy_web/deploy-web.sh
@@ -2,7 +2,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "script is located in " $SCRIPT_DIR
-echo "copping files"
 
+echo "copping files ..."
 cp -rf $SCRIPT_DIR/../build/web $SCRIPT_DIR/../tock-web-app/
-gitcommand= git add . && git commit -m "add build for deploy"
+
+echo "commiting and push ..."
+gitcommand= git add . && git commit -m "automatic commit to deploy build" && git push 
+
