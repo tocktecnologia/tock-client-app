1552d52c9d5a17e3e4a28c0ce51a7f7798c748b1
izaiasemjr <izaiasemjr@gmail.com>
2023-08-04 02:17:00 -0300

    update build number

--- deploy_web/deploy-web.sh
+++ deploy_web/deploy-web.sh
@@ -3,17 +3,21 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "script is located in " $SCRIPT_DIR
 
-echo "copping files ..."
-cp -rf $SCRIPT_DIR/../build/web $SCRIPT_DIR/../tock-web-app/
 
 echo "cd in tock-web-app/"
 cd  $SCRIPT_DIR/../tock-web-app/
-git status
+git checkout main
 git branch 
+
+echo "copping files ..."
+cp -rf $SCRIPT_DIR/../build/web/* $SCRIPT_DIR/../tock-web-app/
+git status
 
 echo "commiting tock-web-app and push ..."
 git add . && git commit -m "automatic commit to deploy build" && git push 
 git status
+git checkout main
+git branch 
 
 echo "update submodule ..."
 cd $SCRIPT_DIR/../
